5d8c35f1ec8755b8c38a61fb055d8a007dce3089
friendo9876 <friendo9876>
2017-02-21 15:35:28 -0500

    Changed all variable names and changed the function from keyup to input

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-var FnameInput = document.getElementById('Fname')
-var LnameInput = document.getElementById('Lname')
-var descInput = document.getElementById('description')
-var emailInput = document.getElementById('user_email')
-var telInput = document.getElementById('user_tel')
-var updateText = function () {
-  var Fname = document.getElementById('Fname').value
-  var Lname = document.getElementById('Lname').value
-  var desc = document.getElementById('description').value
-  var tel = document.getElementById('user_tel').value
-  var email = document.getElementById('user_email').value
-  var name = '<h1>Hi, my name is ' + Fname + ' ' + Lname + '</h1>'
-  var info = '<p>' + desc + '</p>'
-  var interested = '<p>If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email + '</a> '
-  var interested2 = 'or give me a call at <a href="tel:' + tel + '" target="_blank">' + tel + '</a>.</p>'
-  var all = name + info + interested + interested2
-  document.getElementById('preview').innerHTML = all
-  document.getElementById('raw').textContent = all
+var FirstNameInput = document.getElementById('FirstName')
+var LastNameInput = document.getElementById('LastName')
+var DescriptionInput = document.getElementById('Description')
+var EmailInput = document.getElementById('UserEmail')
+var TelInput = document.getElementById('UserTel')
+var UpdateText = function () {
+  var FirstName = FirstNameInput.value
+  var LastName = LastNameInput.value
+  var Description = DescriptionInput.value
+  var Tel = TelInput.value
+  var Email = EmailInput.value
+  var FullName = '<h1>Hi, my name is ' + FirstName + ' ' + LastName + '</h1>'
+  var DescriptionText = '<p>' + Description + '</p>'
+  var EmailText = '<p>If you\'re interested in a date, you can email me at <a href="mailto:' + Email + '" target="_blank">' + Email + '</a> '
+  var TelText = 'or give me a call at <a href="tel:' + Tel + '" target="_blank">' + Tel + '</a>.</p>'
+  var All = FullName + DescriptionText + EmailText + TelText
+  document.getElementById('preview').innerHTML = All
+  document.getElementById('raw').textContent = All
 }
-FnameInput.addEventListener('keyup', updateText)
-LnameInput.addEventListener('keyup', updateText)
-descInput.addEventListener('keyup', updateText)
-telInput.addEventListener('keyup', updateText)
-emailInput.addEventListener('keyup', updateText)
+FirstNameInput.addEventListener('input', UpdateText)
+LastNameInput.addEventListener('input', UpdateText)
+DescriptionInput.addEventListener('input', UpdateText)
+TelInput.addEventListener('input', UpdateText)
+EmailInput.addEventListener('input', UpdateText)
